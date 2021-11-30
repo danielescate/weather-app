ae99168502cacf2cf58481b463424fb1886bf41d
danielescate <daniel.f.escate@gmail.com>
2021-11-30 17:15:06 -0500

    Updates

---
+++ precache-manifest.9b20f12728ce05c3c384fb251842a72c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e38536af3d6d37a4d81777cca66651",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ece0fc501ad0ff0158a6",
+    "url": "/weather-app/static/css/main.ec113ec1.chunk.css"
+  },
+  {
+    "revision": "7077cc7ced5345034841",
+    "url": "/weather-app/static/js/2.50438231.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.50438231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece0fc501ad0ff0158a6",
+    "url": "/weather-app/static/js/main.7eee33c5.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  },
+  {
+    "revision": "9c07387ced2f945034c3e7c84f87aa1b",
+    "url": "/weather-app/static/media/weather-background.9c07387c.gif"
+  }
+]);
